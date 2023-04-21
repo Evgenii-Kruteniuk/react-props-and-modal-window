4f71f6bbe1840d066767d2688cd5314446c192a3
Evgenii-Kruteniuk <muster00100@yandex.ru>
2023-04-21 12:38:01 +0300

    21.04.23

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
+import React from "react";
 import "./App.css";
-import User from "./components/User/User";
-import { Count, Counter } from "./components/Count/Count";
-import Modal from "./components/Modal/Modal";
+import Film from "./components/Film";
 
 function App() {
   return (
     <div className="App">
-      <Modal />
-      <Count />
-      <Counter />
-      <User
-        userName="Женя"
-        age={38}
-        color="красный"
-        isFat={false}
-        bgCard="#79c7c7"
-        textColorCard="white"
+      <Film
+        name="Avatar"
+        src="https://i.pinimg.com/originals/6d/44/82/6d4482ab03ca7de7405144221256edcd.jpg"
+        produser="Джеймс Кэмерон"
+        year={2009}
+        price=" 250$"
       />
-      <User
-        userName="Саша"
-        age={1}
-        color="желтый"
-        isFat={true}
-        bgCard="#9c27b0"
-        textColorCard="green"
+      <Film
+        name="Gladiator"
+        src="https://avatars.dzeninfra.ru/get-zen_doc/5231833/pub_62823156ed0a9814cf129f8f_6282324a19bce16505ab677f/scale_1200"
+        produser="Ридли Скотт"
+        year={2000}
+        price=" 200$"
+        likeFilm
       />
-      <User
-        userName="Маша"
-        age={8}
-        color="зеленый"
-        isFat={false}
-        bgCard="#11a8d4"
-        textColorCard="yellow"
-        isShow
+      <Film
+        name="Jurassic park"
+        src="https://get.wallhere.com/photo/1920x1080-px-adventure-dinosaur-fantasy-fi-film-jurassic-movie-park-poster-sci-1671017.jpg"
+        produser="Стивен Спилберг"
+        year={1993}
+        price=" 85$"
+      />
+      <Film
+        name="The matrix"
+        src="https://papik.pro/uploads/posts/2021-10/1634581776_17-papik-pro-p-poster-matritsa-19.jpg"
+        produser="Лилли Вачовски"
+        year={1999}
+        price=" 90$"
       />
     </div>
   );
---
+++ src/components/Film.jsx
@@ -0,0 +1,53 @@
+import "./Film.css";
+import React, { Component } from "react";
+
+class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: false,
+    };
+  }
+
+  openModal = () => {
+    this.setState({
+      isShow: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isShow: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>{this.props.name}</h1>
+        <img src={this.props.src} alt={this.props.name} />
+        <p>Режиссер фильма: {this.props.produser}</p>
+        <p>Год выхода фильма: {this.props.year}</p>
+        <p>Цена:{this.props.price}</p>
+        {this.props.likeFilm ? (
+          <p>Это мой любимый фильм. Рекомендую!!</p>
+        ) : null}
+        <button onClick={this.openModal}>Купить</button>
+
+        <div
+          className={`modal - window - wrapper ${
+            this.state.isShow ? null : "modal-hide"
+          }`}
+        >
+          <div className="modal-window">
+            <img src={this.props.src} alt={this.props.name} />
+            <p>Вы должны оплатить:{this.props.price}</p>
+            <button onClick={this.closeModal}>Вернуться к покупке</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Film;
